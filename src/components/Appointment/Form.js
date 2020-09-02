f0ed6f5396ad6c60fe9b6a872cdb02e49d733a89
Angel Ho <angel.ho@ryerson.ca>
2020-09-01 21:45:01 -0400

    added edit, create forms

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import InterviewerList from "../InterviewerList";
+import Button from "../Button";
+
+
+export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
+  const reset = () => {
+    setName("");
+    setError("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
+  const validate = () => {
+    if (!name) {
+      setError("Cannot leave student name blank");
+      return;
+    }
+    if (!interviewer) {
+      setError("Please select an interviewer.");
+      return;
+    }
+    setError("");
+    props.onSave(name, interviewer);
+  }
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name={name}
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(event) => setName(event.target.value)}
+            onSubmit={(event) => event.preventDefault()}
+          />
+        </form>
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={validate}>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "components/InterviewerList.scss";
-import classnames from "classnames";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
-  const interviewer = props.interviewers.map(interviewer => {
+  const interviewers = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
         key={interviewer.id}
@@ -20,7 +19,7 @@ export default function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
 
   )
