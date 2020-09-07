79a1b85d628ed466211374452fb82ca95b3215b5
Angel Ho <angel.ho@ryerson.ca>
2020-09-06 21:10:05 -0400

    implemented separation of concerns with useApplicationData, imported application API data and updating spots remaining

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,69 +1,17 @@
-import React, { useState, useEffect } from "react";
-import DayList from './DayList';
+import React from "react";
+import DayList from "./DayList";
 import Appointment from './Appointment';
-import axios from 'axios';
 import "components/Application.scss";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from '../helpers/selectors.js';
-
+import useApplicationData from 'hooks/useApplicationData';
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  const setDay = day => setState({ ...state, day });
-
-  function bookInterview(id, interview) { // this interview is greyed out, but i thought we used it in line 22?
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview } //It's because on index.js, this interview input will be the interview object that will be updating the appointment data 
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment //Here, we are assigning the updated appointment to the appointments objects
-    };
-    //call setState wtih the new state object (do promise here)
-    return axios
-      .put(`http://localhost:8001/api/appointments/${id}`, appointment)
-      .then(() => setState({ ...state, appointments }))
-    // .catch(error => console.log(error));
-  }
-  //When we cancel an interview it will have its value set to null.If the interview is null and we are still in the SHOW mode then we may get a TypeError.
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios
-      .delete(`http://localhost:8001/api/appointments/${id}`)
-      .then(() => setState({ ...state, appointments }))
-    // .catch(error => console.log(error));
-  }
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
-    ]).then(all => {
-      setState(prev => ({
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data
-      }))
-    })
-    //     .catch(error => console.log(error));
-  }, []);
-
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   const interviewers = getInterviewersForDay(state, state.day);
   const appointments = getAppointmentsForDay(state, state.day);
@@ -110,5 +58,5 @@ export default function Application(props) {
         <Appointment key="last" time="5pm" />
       </section>
     </main>
-  );
+  )
 };
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "components/Application.scss";
+
+export default function useApplicationData() {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ]).then(all => {
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }))
+    })
+  }, []);
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview } // on index.js, this interview input will be the interview object that will be updating the appointment data 
+    }
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment //Here, we are assigning the updated appointment to the appointments objects
+    };
+    //call setState wtih the new state object (do promise here)
+    return axios.put(`http://localhost:8001/api/appointments/${id}`, appointment)
+      .then(() => axios.get(`http://localhost:8001/api/days`))
+
+      .then((response) => {
+        const days = response.data
+        console.log(response)
+        setState({ ...state, appointments, days })
+      })
+  }
+  //When we cancel an interview it will have its value set to null.If the interview is null and we are still in the SHOW mode then we may get a TypeError.
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
+      .then(() => axios.get(`http://localhost:8001/api/days`))
+      .then((response) => {
+        const days = response.data
+        setState({ ...state, appointments, days })
+      })
+  }
+
+  //returning object of four keys
+  return {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  }
+} 
